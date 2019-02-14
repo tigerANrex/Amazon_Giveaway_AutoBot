b547ecf928cb06812dda9b3a841fdc4106a8be63
tigerANrex <nguyen11@memphis.edu>
2019-02-14 12:33:23 -0600

    Goes to the giveaway page

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
 
+amazon_url = "https://www.amazon.com"
 url = "https://www.amazon.com/giveaways"
-r = requests.get(url).text
+r = requests.get(url)
+soup = BeautifulSoup(r.text, 'html.parser')
 
-print(r)
-# soup = BeautifulSoup(r)
+giveaway_href = soup.findAll("a", {"class": "a-button-text bxc-button-text"})[0].get('href')
+giveaway_link = "".join((amazon_url, giveaway_href))
+r.close()
+
+r = requests.get(giveaway_link)
+soup = BeautifulSoup(r.text, 'html.parser') # Why this no work
+items = soup.findAll("li", {"class": "a-section a-spacing-base listing-item"})[0]
+print(items)
