5d60ab1b50bd99320d8c72372f99b5fb56d03944
tigerANrex <nguyen11@memphis.edu>
2019-02-19 12:27:31 -0600

    Added Login

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 amazon_url = "https://www.amazon.com"
+amazon_login = "https://www.amazon.com/gp/sign-in.html"
 url = "https://www.amazon.com/giveaways"
 browser = webdriver.Chrome()
+browser.get(amazon_login)                                                       # Login to Amazon
+while(True):
+    try:
+        wait = WebDriverWait(browser, 5).until(EC.title_is("Your Account"))
+        break
+    except Exception as e:
+        pass
+
 browser.get(url)
 soup = BeautifulSoup(browser.page_source, 'html.parser')
 
@@ -12,25 +24,13 @@ giveaway_link = "".join((amazon_url, giveaway_href))
 soup.decompose()
 
 browser.get(giveaway_link)
+time.sleep(2)
 soup = BeautifulSoup(browser.page_source, "html.parser")
-item_list = soup.findAll("a", {"class": "a-link-normal item-link"})
-# print(item_list[0].get('href'))
-for items in item_list:
-    item_link = "".join((amazon_url, items.get('href')))
-    print(item_link)                                          # Prints the URL for each items
-    # browser.get(item_link)
-    # soup2 = BeautifulSoup(browser.page_source, 'html.parser')
-
-    # lose_text = soup.findAll("span", {"class": "a-size-base-plus a-color-secondary qa-giveaway-result-text a-text-bold"})[0]
-    # print(lose_text)
-
-
 ###Avery's code
 
-items = soup.findAll("li", {"class": "a-section a-spacing-base listing-item"})
+items = soup.findAll("a", {"class": "a-link-normal item-link"})
 for i in items:
     requirement = str(i.find('span',{"class": "a-text-bold"}))[26:-7]
-    print(requirement)
-    #if  is in requirement:
-     #   href = i.get('href')
-      #  print(href)
+    if requirement == "No entry requirement":
+        item_link = "".join((amazon_url, i.get('href')))
+        print(item_link)
