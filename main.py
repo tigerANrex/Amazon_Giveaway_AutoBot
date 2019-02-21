2f59acb154745fd51c839e1454edc78275815530
Neron <2.clary.avery@gmail.com>
2019-02-20 18:00:40 -0600

    Changed sleep and added #!

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+#! /usr/bin/python
+
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 
 amazon_url = "https://www.amazon.com"
 amazon_login = "https://www.amazon.com/gp/sign-in.html"
@@ -21,14 +22,18 @@ soup = BeautifulSoup(browser.page_source, 'html.parser')
 
 giveaway_href = soup.findAll("a", {"class": "a-button-text bxc-button-text"})[0].get('href')
 giveaway_link = "".join((amazon_url, giveaway_href))
+
 soup.decompose()
-
-browser.get(giveaway_link)
-time.sleep(2)
+browser.get(giveaway_link) # Goes to givwaway page
+while(True):
+    try: # Waits till the page is loaded (this is because the site is coded in react)
+        wait = WebDriverWait(browser, 1).until(EC.title_is("Amazon Giveaways"))
+        break
+    except Exception as e:
+        pass 
 soup = BeautifulSoup(browser.page_source, "html.parser")
-###Avery's code
 
-items = soup.findAll("a", {"class": "a-link-normal item-link"})
+items = soup.findAll("a", {"class": "a-link-normal item-link"}) # Looks for the sepreate giveaways
 for i in items:
     requirement = str(i.find('span',{"class": "a-text-bold"}))[26:-7]
     if requirement == "No entry requirement":
